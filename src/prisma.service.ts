6ed5cb0e71e75fa8bc158119456739b93d5616d8
Nicolas Carraro <e12202357@student.tuwien.ac.at>
2025-10-30 17:58:25 +0100

    firstendpoints

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,8 +1,10 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { INestApplication, Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient
+  implements OnModuleInit {
+
   async onModuleInit() {
     await this.$connect();
   }
@@ -10,6 +12,6 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   async enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', async () => {
       await app.close();
-    });
+    });    
   }
 }
