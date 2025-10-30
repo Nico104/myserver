d93cfa067b0384efa01906115727a4fbc768d3dd
Nico <nicosmail>
2025-10-30 16:39:29 +0000

    todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DiaryModule } from './diary/diary.module';
 import { TodoModule } from './todo/todo.module';
+import { TodolistModule } from './todolist/todolist.module';
 
 @Module({
-  imports: [DiaryModule, TodoModule],
+  imports: [DiaryModule, TodoModule, TodolistModule],
   controllers: [AppController],
   providers: [AppService],
 })
