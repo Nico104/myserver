74eb926e585b6f958f8fd99303f88b9727a1b8a9
Nico <nicosmail>
2025-10-31 19:33:33 +0000

    g notes plans

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { DiaryModule } from './diary/diary.module';
 import { TodosModule } from './todo/todo.module';
 import { TodolistModule } from './todolist/todolist.module';
+import { NotesController } from './notes/notes.controller';
+import { NotesService } from './notes/notes.service';
+import { NotesModule } from './notes/notes.module';
+import { PlansModule } from './plans/plans.module';
 
 @Module({
-  imports: [DiaryModule, TodosModule, TodolistModule],
+  imports: [DiaryModule, TodosModule, TodolistModule, NotesModule, PlansModule],
+  controllers: [NotesController],
+  providers: [NotesService],
   // controllers: [AppController],
   // providers: [AppService],
 })
